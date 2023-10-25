529d850d5b164e4f0eed52ed2b65e1b649019309
Jestoni Cruz <jestoni.estrada.cruz@gmail.com>
2023-10-25 20:24:33 +0800

    Add web dev

---
+++ Scripts/script.js
@@ -0,0 +1,52 @@
+
+const body = document.body
+body.append("Web Dev")
+
+/*
+let i = 100
+while(i >= 1){
+    i--
+     if (i % 2 === 0){
+        console.log(i)
+     }
+}
+
+for(let i = 0; i <= 100; i++){
+    if(i % 2 === 1){
+        console.log(i)
+    }
+}
+
+const age = 12
+
+if(age >= 1 && age <= 12 ){
+    alert ("You are children!")
+}else if(age >= 13 && age <= 21){
+    alert ("You are Teenager!")
+}else if(age >= 22 && age <= 59){
+    alert ("You are Adult!")
+}else if(age >= 60){
+    alert ("You are Senior!")
+}else{
+    alert("Invalid age")
+}
+
+const firstNum = 10
+const secondNum = 11
+
+function funcDiff(firstDiff,secondDiff){
+    return firstDiff - secondDiff
+}
+
+function funcProduct(firstPro, secondPro){
+    return firstPro * secondPro
+}
+
+function funcQuotient(firstQuotient, secondQuotient){
+    return firstQuotient / secondQuotient
+}
+
+console.log(funcDiff(firstNum, secondNum))
+console.log(funcProduct(firstNum,secondNum))
+console.log(funcQuotient(firstNum,secondNum))
+*/
